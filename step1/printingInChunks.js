840e281d861ffe866104edfb2a8ac8c8e0a0b8b1
Calvin Yah <calvin.yah@gmail.com>
2018-02-19 17:29:08 +0000

    First Commit

---
+++ step1/printingInChunks.js
@@ -0,0 +1,27 @@
+  var https = require('https');
+
+function getAndPrintHTMLChunks () {
+
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step1.html'
+  };
+
+  https.get(requestOptions, function (response) {	
+  	response.setEncoding('utf8');
+
+    console.log('Status Code: ', response.statusCode);
+    console.log('Headers: ', response.headers);
+
+  	var output = "";
+
+  	response.on('data', function (data) {
+       console.log('Chunk length: ', data + '\n');
+
+        // append our data to our output buffer
+        output += data;
+    });
+  });
+};
+
+getAndPrintHTMLChunks();
